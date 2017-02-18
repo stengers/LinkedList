1c2be0ea6f5cf83a0d384d6434dff3be94dfdb55
stengers <spencerstenger@gmail.com>
2017-02-18 11:01:04 -0800

    First commit

---
+++ LinkedList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LinkedList
+//
+//  Created by Spencer Stenger on 2/18/17.
+//  Copyright © 2017 Spencer Stenger. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ LinkedList/node.swift
@@ -0,0 +1,9 @@
+//
+//  node.swift
+//  LinkedList
+//
+//  Created by Spencer Stenger on 2/18/17.
+//  Copyright © 2017 Spencer Stenger. All rights reserved.
+//
+
+import Foundation
